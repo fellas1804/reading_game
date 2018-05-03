99ca0b883cb61cf22ba9bdc10a2032097ab83f8f
Jon Rea <jonarea@gmail.com>
2018-05-03 11:31:36 -0400

    have basic speech recognition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './App.css'
 import AudioPlayer from './components/AudioPlayer'
+import SpeechRecognition from 'react-speech-recognition'
+
+const propTypes = {
+  transcript: PropTypes.string,
+  resetTranscript: PropTypes.func,
+  browserSupportsSpeechRecognition: PropTypes.bool
+}
 
 class App extends Component {
   render() {
+    const { transcript, resetTranscript, browserSupportsSpeechRecognition } = this.props
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">The Reading Game</h1>
+          <p>{transcript}</p>
         </header>
         <AudioPlayer />
       </div>
@@ -15,4 +25,5 @@ class App extends Component {
   }
 }
 
-export default App
+App.propTypes = propTypes
+export default SpeechRecognition(App)
